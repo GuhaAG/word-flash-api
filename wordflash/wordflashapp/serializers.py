fcf187da4cb7bf1b1abaeac4dd607b430486f158
Arghya Guha <guha.agt@gmail.com>
2019-09-17 20:45:16 +0900

    fixed model-less serializer to get working api with static mock data

--- wordflash/wordflashapp/serializers.py
+++ wordflash/wordflashapp/serializers.py
@@ -1,9 +1,7 @@
 from rest_framework import serializers
-from . import WordPair
 
 
-class WordPairSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        id = serializers.IntegerField(read_only=True)
-        key_en = serializers.CharField(max_length=256)
-        key_jp = serializers.CharField(max_length=256)
+class WordPairSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    key_en = serializers.CharField(max_length=256)
+    key_jp = serializers.CharField(max_length=256)
