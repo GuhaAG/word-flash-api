1343527b4532de175be9dce2c86fe92f38813773
Arghya Guha <guha.agt@gmail.com>
2019-09-17 23:28:29 +0900

    issue #2, return word pairs from downloaded csv file

--- wordflash/wordflashapp/serializers.py
+++ wordflash/wordflashapp/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 
 
-class WordPairSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
+class WordPairSerializer(serializers.Serializer):    
     key_en = serializers.CharField(max_length=256)
     key_jp = serializers.CharField(max_length=256)
